4f5e3e770cbc5d45d35c449320f2c23ae02f2484
JP3tri3 <JP3tri3@gmail.com>
2022-05-26 15:19:53 -0400

    added formatting

--- src/services/alerts/handle-alerts.js
+++ src/services/alerts/handle-alerts.js
@@ -6,28 +6,29 @@ module.exports = async (symbol, strategyName, alertPositions, alerts) => {
     try {
         // time is set to defaults, TODO: Add user specific time settings
         const currentTime = getFormattedTime('GMT-0500', 24);
+        symbol = symbol.toUpperCase();
+        strategyName = strategyName.charAt(0).toUpperCase() + strategyName.slice(1),
+            alertPositions.forEach(async posAction => {
+                alerts.forEach(async alert => {
+                    const alertType = alert.type;
+                    const address = alert.address;
+                    const message = createAlertMessage(alert, posAction);
 
-        alertPositions.forEach(async posAction => {
-            alerts.forEach(async alert => {
-                const alertType = alert.type;
-                const address = alert.address;
-                const message = createAlertMessage(alert, posAction);
+                    switch (alertType) {
+                        case 'discord':
+                            await services.discord(symbol, strategyName, posAction, message, address, currentTime);
+                            break;
 
-                switch (alertType) {
-                    case 'discord':
-                        await services.discord(symbol, strategyName, posAction, message, address, currentTime);
-                        break;
+                        case 'email':
+                            await services.email(symbol, strategyName, posAction, message, address, currentTime);
+                            break;
 
-                    case 'email':
-                        await services.email(symbol, strategyName, posAction, message, address, currentTime);
-                        break;
-
-                    case 'bot':
-                        console.log('send bot notification');
-                        break;
-                };
+                        case 'bot':
+                            console.log('send bot notification');
+                            break;
+                    };
+                });
             });
-        });
     }
 
     catch (err) {
