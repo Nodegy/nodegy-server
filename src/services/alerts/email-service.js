44d01cd1398c0df298e3d2537a2f2d00acaef4e6
JP3tri3 <JP3tri3@gmail.com>
2022-05-26 12:14:19 -0400

    imported email-service

--- src/services/alerts/email-service.js
+++ src/services/alerts/email-service.js
@@ -1,26 +1,12 @@
-const nodemailer = require('nodemailer');
 const { handleInternalError } = require('../../utils/internal-handlers/index');
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.FROM_EMAIL_ACCOUNT,
-        pass: process.env.EMAIL_PASSWORD
-    }
-});
+const emailService = require('../email/send-email-service');
 
 module.exports = async (symbol, name, posAction, message, address) => {
     try {
-        const mailOptions = {
-            from: process.env.FROM_EMAIL_ACCOUNT,
-            to: address,
-            subject: `Nodegy Alert! Strategy: ${name}, Symbol: ${symbol}`,
-            text: `Strategy: ${name}\nAction: ${posAction}\nSymbol: ${symbol}Message: ${message}`
-        };
-
-        await transporter.sendMail(mailOptions);
-
-
+        const subject = `Nodegy Alert! Strategy: ${name}, Symbol: ${symbol}`;
+        const msgBody = `Strategy: ${name}\nAction: ${posAction}\nSymbol: ${symbol}Message: ${message}`;
+        const confirm = await emailService(address, subject, msgBody);
+        return Promise.resolve(confirm);
     } catch (err) {
         handleInternalError({
             message: 'Error in Email Service',
