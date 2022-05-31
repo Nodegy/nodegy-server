9485893ff83b05a0206c757199e8fa9ec1410f08
JP3tri3 <JP3tri3@gmail.com>
2022-05-31 16:04:09 -0400

    test ws

--- src/controllers/websocket/connect.js
+++ src/controllers/websocket/connect.js
@@ -5,5 +5,6 @@ module.exports = async (req, res) => {
     const eid = req.cookies.eid;
 
     console.log('\n\n\nCONNECTED TEST SUCCESSFUL\n\n\n')
+    console.log('req: ', req)
     return res.status(200).send({ message: 'connection success' })
 };
