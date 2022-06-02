662bf7759e3c7f8d8cc31bd0e4606420442b14fb
JP3tri3 <JP3tri3@gmail.com>
2022-06-02 17:41:55 -0400

    ws test deploy

--- src/controllers/websocket/connect.js
+++ src/controllers/websocket/connect.js
@@ -2,11 +2,10 @@ const service = 'websocket connect';
 const { addClient } = require('../../services/websocket/ws-ids');
 
 module.exports = (req, res) => {
-    console.log('-------- connecting: ----------');
-    console.log('BODY TEST: ', req.body)
-    // const connectionId = req.body.connectionId;
+    const eid = req.body.eid;
+    const connectionId = req.body.connectionId;
 
-    // addClient(connectionId, eid);
+    addClient(connectionId, eid);
 
     return res.status(200).send({ status: 'connected' });
 };
--- src/controllers/websocket/disconnect.js
+++ src/controllers/websocket/disconnect.js
@@ -1,12 +1,12 @@
-// const { removeClient } = require('../../services/websocket/ws-ids');
+const { removeClient } = require('../../services/websocket/ws-ids');
 const service = 'websocket disconnect';
 
 module.exports = (req, res) => {
+    console.log('req.body: ', req.body);
     // const eid = req.cookies.eid;
     // const connectionId = req.body.connectionId;
 
     // removeClient(connectionId, eid);
-    console.log('disconnected')
 
     return res.status(200).send({ status: 'disconnected' });
 };
