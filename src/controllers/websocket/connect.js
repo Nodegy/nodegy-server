fc5b2c80e7aa89a6b1388a18b710015c6d3e03a4
JP3tri3 <JP3tri3@gmail.com>
2022-06-02 15:35:31 -0400

    test deploy

--- src/controllers/websocket/connect.js
+++ src/controllers/websocket/connect.js
@@ -6,8 +6,8 @@ module.exports = (req, res) => {
     // console.log('req: ', req);
     // console.log('typeof: ', typeof req);
 
-    // const connectionId = req.body.connectionId;
-    // console.log('connectionId: ', connectionId);
+    const connectionId = req.body.connectionId;
+    console.log('connectionId: ', connectionId);
 
     // const domain = Object.keys(req.body).includes('domain') ? req.body.domain : null;
     // const stage = Object.keys(req.body).includes('stage') ? req.body.stage : null;
